fccb90a9a8c3cf94713d25a5d4656cb74670e998
Andrea <andrea.rosati87@yahoo.it>
2019-05-04 19:32:38 +0200

    easy stuff done <(''<)  <( ' ' )>  (> '')>

--- ParrotronicAndroid/app/src/main/java/com/example/parrotronicandroid/MainActivity.java
+++ ParrotronicAndroid/app/src/main/java/com/example/parrotronicandroid/MainActivity.java
@@ -2,12 +2,63 @@ package com.example.parrotronicandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.SeekBar;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
+
+@EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+
+    @ViewById(R.id.mouthValueText)
+    TextView mouthValueTextView;
+
+    @ViewById(R.id.mouthBar)
+    SeekBar mouthBar;
+
+    @ViewById(R.id.eyesSwitch)
+    Switch eyes;
+
+    @AfterViews
+    void AfterViews(){
+        eyes.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                eyesSwitchPressed();
+            }
+        });
+
+        mouthBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                mouthValueTextView.setText(""+ progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
+
+
+    private void eyesSwitchPressed()
+    {
+        Toast.makeText(this, "premuto", Toast.LENGTH_LONG).show();
+    }
+
+
+
 }
